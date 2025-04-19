311555e8a445d7adb752cbacab1e49784793b6ae
saraivaa.me <wesley-saraiva@outlook.com>
2025-04-19 18:48:06 -0300

    (STYLE: HeroSection.tsx): alteracao na capa da hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,10 +10,31 @@ const HeroSection = () => {
   };
   return (
     <section
-      className="relative min-h-screen flex items-center pt-16 bg-primary"
+      className="relative min-h-screen flex items-center pt-16"
       id="home"
+      style={{
+        backgroundImage: "url('/src/assets/images/capa-hero.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
     >
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
+        <div className="flex justify-center mb-12 md:w-1/2 md:mb-0 md:justify-start">
+          <div className="relative">
+            <div className="w-64 h-64 mt-12 md:mt-0 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-gold shadow-2xl">
+              <img
+                src={perfil}
+                alt="Dra. Elaine Rute Araújo"
+                className="w-full object-cover"
+              />
+            </div>
+            <div className="absolute -right-6 -bottom-6 py-2 px-4 rounded-lg shadow-lg bg-gold text-navy">
+              <p className="font-bold"> OAB/CE 50.397 </p>
+            </div>
+          </div>
+        </div>
+
         <div className="text-center text-white md:w-1/2 md:text-left">
           <h1 className="text-4xl font-bold mb-4 md:text-5xl lg:text-6xl">
             Dra. Elaine Rute Araújo
@@ -39,21 +60,6 @@ const HeroSection = () => {
             >
               Áreas de Atuação
             </a>
-          </div>
-        </div>
-
-        <div className="flex justify-center mt-12 md:w-1/2 md:mt-0 md:justify-end">
-          <div className="relative">
-            <div className="w-64 h-64 mt-12 md:mt-0 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-gold shadow-2xl">
-              <img
-                src={perfil}
-                alt="Dra. Elaine Rute Araújo"
-                className="w-full object-cover"
-              />
-            </div>
-            <div className="absolute -right-6 -bottom-6 py-2 px-4 rounded-lg shadow-lg bg-gold text-navy">
-              <p className="font-bold"> OAB/CE 50.397 </p>
-            </div>
           </div>
         </div>
       </div>
